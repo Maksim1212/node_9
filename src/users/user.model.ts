6ea9f321bf75112cf87ad8a79290538f92f48374
maksim1212 <mr.frost201611@gmail.com>
2020-04-22 21:53:37 +0300

    fix(users module): was added phone to user.model
    was added phone to user.model

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -4,6 +4,7 @@ export const UserSchema = new mongoose.Schema({
     firstName:{ type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
+    phone: {type: String, required: true},
     isAdmin: {type: Boolean, required: true},
     verified: {type: Boolean, required: true},
 });
